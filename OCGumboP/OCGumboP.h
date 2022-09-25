1fb59be2fa12a7d51f719e853394ffff23a2fa95
Garenge <garenge@outlook.com>
2022-09-25 16:10:29 +0800

    Initial Commit

---
+++ OCGumboP/OCGumboP.h
@@ -0,0 +1,18 @@
+//
+//  OCGumboP.h
+//  OCGumboP
+//
+//  Created by 鹏鹏 on 2022/9/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OCGumboP.
+FOUNDATION_EXPORT double OCGumboPVersionNumber;
+
+//! Project version string for OCGumboP.
+FOUNDATION_EXPORT const unsigned char OCGumboPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCGumboP/PublicHeader.h>
+
+
